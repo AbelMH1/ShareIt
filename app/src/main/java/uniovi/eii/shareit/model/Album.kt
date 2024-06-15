52eed4a589782b67484875ca1936773d7c8e8188
Abel <abelmhov7l@gmail.com>
2024-06-15 21:40:34 +0200

    Añadido viewmodel y comprobación de datos para las pantallas de información sobre un álbum

---
+++ app/src/main/java/uniovi/eii/shareit/model/Album.kt
@@ -0,0 +1,55 @@
+package uniovi.eii.shareit.model
+
+import com.google.android.gms.maps.model.LatLng
+import java.util.Date
+
+data class Album(
+    var name: String = "",
+    var startDate: Date? = null,
+    var endDate: Date? = null,
+    var location: LatLng? = null,
+    var shared: Boolean = false,
+    var membersImagesPermission: String? = null,
+    var membersChatPermission: String? = null,
+    var guestsImagesPermission: String? = null,
+    var guestsChatPermission: String? = null,
+    var invitationLinkEnabled: Boolean = false,
+    var invitationLink: String? = null,
+    var participants: MutableList<String>? = null
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Album
+
+        if (name != other.name) return false
+        if (startDate != other.startDate) return false
+        if (endDate != other.endDate) return false
+        if (location != other.location) return false
+        if (shared != other.shared) return false
+        if (membersImagesPermission != other.membersImagesPermission) return false
+        if (membersChatPermission != other.membersChatPermission) return false
+        if (guestsImagesPermission != other.guestsImagesPermission) return false
+        if (guestsChatPermission != other.guestsChatPermission) return false
+        if (invitationLinkEnabled != other.invitationLinkEnabled) return false
+        if (invitationLink != other.invitationLink) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = name.hashCode()
+        result = 31 * result + (startDate?.hashCode() ?: 0)
+        result = 31 * result + (endDate?.hashCode() ?: 0)
+        result = 31 * result + (location?.hashCode() ?: 0)
+        result = 31 * result + shared.hashCode()
+        result = 31 * result + (membersImagesPermission?.hashCode() ?: 0)
+        result = 31 * result + (membersChatPermission?.hashCode() ?: 0)
+        result = 31 * result + (guestsImagesPermission?.hashCode() ?: 0)
+        result = 31 * result + (guestsChatPermission?.hashCode() ?: 0)
+        result = 31 * result + invitationLinkEnabled.hashCode()
+        result = 31 * result + (invitationLink?.hashCode() ?: 0)
+        return result
+    }
+}
--- app/src/main/java/uniovi/eii/shareit/ui/album/information/display/AlbumInformationGeneralFragment.kt
+++ app/src/main/java/uniovi/eii/shareit/ui/album/information/display/AlbumInformationGeneralFragment.kt
@@ -1,14 +1,23 @@
 package uniovi.eii.shareit.ui.album.information.display
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.material.textfield.TextInputLayout
 import uniovi.eii.shareit.R
 import uniovi.eii.shareit.databinding.FragmentAlbumInformationGeneralBinding
+import uniovi.eii.shareit.model.Album
 import uniovi.eii.shareit.ui.album.information.AlbumInformationViewModel
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class AlbumInformationGeneralFragment : Fragment() {
 
@@ -25,47 +34,35 @@ class AlbumInformationGeneralFragment : Fragment() {
     ): View {
         _binding = FragmentAlbumInformationGeneralBinding.inflate(inflater, container, false)
         enableEdition(false)
+        setUpListeners()
+        viewModel.album.observe(viewLifecycleOwner) {
+            updateUI(it)
+        }
+        return binding.root
+    }
 
+    private fun setUpListeners() {
         binding.editFAB.setOnClickListener {
             enableEdition(true)
             binding.editFAB.hide()
             binding.saveFAB.show()
         }
         binding.saveFAB.setOnClickListener {
+            val album = checkData() ?: return@setOnClickListener
             enableEdition(false)
             binding.editFAB.show()
             binding.saveFAB.hide()
+            viewModel.saveAlbumInfo(album)
         }
-
         binding.dateToggleButton.addOnButtonCheckedListener { _, checkedId, isChecked ->
-            if (isChecked) {
-                when (checkedId) {
-                    R.id.toggleNone -> {
-                        binding.dateStartLayout.visibility = View.GONE
-                        binding.dateEndLayout.visibility = View.GONE
-                    }
-
-                    R.id.toggleSingle -> {
-                        binding.dateStartLayout.visibility = View.VISIBLE
-                        binding.dateEndLayout.visibility = View.GONE
-                    }
-
-                    R.id.toggleRange -> {
-                        binding.dateStartLayout.visibility = View.VISIBLE
-                        binding.dateEndLayout.visibility = View.VISIBLE
-                    }
-                }
-            }
+            if (isChecked) toggleDatesEditTexts(checkedId)
         }
-
         binding.dateStartLayout.setEndIconOnClickListener {
             // Show date picker
         }
-
         binding.dateEndLayout.setEndIconOnClickListener {
             // Show date picker
         }
-
         binding.switchLocationSelection.setOnCheckedChangeListener { _, isChecked ->
             when (isChecked) {
                 true -> {
@@ -77,13 +74,35 @@ class AlbumInformationGeneralFragment : Fragment() {
                 }
             }
         }
+        // To remove error messages
+        binding.nameEditText.addTextChangedListener(ValidationTextWatcher(binding.nameLayout))
+        binding.dateStartEditText.addTextChangedListener(ValidationTextWatcher(binding.dateStartLayout))
+        binding.dateEndEditText.addTextChangedListener(ValidationTextWatcher(binding.dateEndLayout))
+    }
 
-        return binding.root
+    private fun toggleDatesEditTexts(checkedId: Int) {
+        when (checkedId) {
+            R.id.toggleNone -> {
+                binding.dateStartLayout.visibility = View.GONE
+                binding.dateEndLayout.visibility = View.GONE
+            }
+
+            R.id.toggleSingle -> {
+                binding.dateStartLayout.visibility = View.VISIBLE
+                binding.dateEndLayout.visibility = View.GONE
+            }
+
+            R.id.toggleRange -> {
+                binding.dateStartLayout.visibility = View.VISIBLE
+                binding.dateEndLayout.visibility = View.VISIBLE
+            }
+        }
     }
 
     override fun onResume() {
         super.onResume()
         binding.editFAB.show()
+        updateUI(viewModel.getAlbumInfo())
     }
 
     override fun onDestroyView() {
@@ -117,6 +136,94 @@ class AlbumInformationGeneralFragment : Fragment() {
             binding.dateEndLayout.endIconDrawable = null
             binding.editFAB.show()
             binding.saveFAB.hide()
+        }
+    }
+
+    private fun checkData(): Album? {
+        val albumData = viewModel.getAlbumInfo()
+        albumData.name = validateTextField(binding.nameLayout) ?: return null
+        when (binding.dateToggleButton.checkedButtonId) {
+            R.id.toggleNone -> {
+                albumData.startDate = null
+                albumData.endDate = null
+            }
+            R.id.toggleSingle -> {
+                albumData.startDate = validateDate(binding.dateStartLayout) ?: return null
+                albumData.endDate = null
+            }
+            R.id.toggleRange -> {
+                val dateStart = validateDate(binding.dateStartLayout) ?: return null
+                val dateEnd = validateDate(binding.dateEndLayout) ?: return null
+                if (!dateEnd.after(dateStart)) {
+                    binding.dateEndLayout.error =
+                        resources.getString(R.string.err_invalid_later_date)
+                    binding.dateEndEditText.requestFocus()
+                    return null
+                }
+                albumData.startDate = dateStart
+                albumData.endDate = dateEnd
+            }
+        }
+        if (binding.switchLocationSelection.isChecked) {
+            // TODO: Obtener ubicación del mapa
+            albumData.location = LatLng(0.0, 0.0)
+        } else albumData.location = null
+        return albumData
+    }
+
+    private fun validateDate(etLayout: TextInputLayout): Date? {
+        val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+        sdf.isLenient = false
+        val date: Date? = try {
+            sdf.parse(etLayout.editText?.text.toString().trim())
+        } catch (e: ParseException) {
+            etLayout.error = resources.getString(R.string.err_invalid_date)
+            etLayout.editText?.requestFocus()
+            return null
+        }
+        return date
+    }
+
+    private fun validateTextField(etLayout: TextInputLayout): String? {
+        val str = etLayout.editText?.text?.toString()?.trim()
+        if (str.isNullOrBlank()) {
+            etLayout.error = resources.getString(R.string.err_empty_field)
+            etLayout.editText?.requestFocus()
+            return null
+        }
+        return str
+    }
+
+    private fun updateUI(album: Album) {
+        binding.albumName.text = album.name
+        binding.nameEditText.setText(album.name)
+        binding.switchLocationSelection.isChecked = album.location != null
+        if (album.location != null) {
+            // TODO: Establecer ubicación en el mapa
+        }
+        val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+        sdf.isLenient = false
+        if (album.endDate != null && album.startDate != null) {
+            binding.dateStartEditText.setText(sdf.format(album.startDate!!))
+            binding.dateEndEditText.setText(sdf.format(album.endDate!!))
+            binding.dateToggleButton.check(R.id.toggleRange)
+        } else if (album.startDate != null) {
+            binding.dateStartEditText.setText(sdf.format(album.startDate!!))
+            binding.dateEndEditText.setText("")
+            binding.dateToggleButton.check(R.id.toggleSingle)
+        } else {
+            binding.dateStartEditText.setText("")
+            binding.dateEndEditText.setText("")
+            binding.dateToggleButton.check(R.id.toggleNone)
+        }
+    }
+
+    private class ValidationTextWatcher constructor(private val etLayout: TextInputLayout) :
+        TextWatcher {
+        override fun beforeTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {}
+        override fun onTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {}
+        override fun afterTextChanged(editable: Editable) {
+            etLayout.error = null
         }
     }
 }
