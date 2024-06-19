c07cdee6e7d607edbbbf52b8bd814746010889ce
Abel <abelmhov7l@gmail.com>
2024-06-19 04:00:05 +0200

    Añadida Data class para las Imágenes y dimensiones estándar para las fotos de perfil

---
+++ app/src/main/java/uniovi/eii/shareit/model/Image.kt
@@ -0,0 +1,41 @@
+package uniovi.eii.shareit.model
+
+import android.os.Parcel
+import android.os.Parcelable
+import java.util.Date
+
+data class Image(
+    var author : String = "",
+    var imagePath : String = "",
+    var creationDate: Date = Date(),
+    var likes : List<String> = emptyList()
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        Date(parcel.readLong()),
+        parcel.createStringArrayList().orEmpty()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(author)
+        parcel.writeString(imagePath)
+        parcel.writeLong(creationDate.time)
+        parcel.writeStringList(likes)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Image> {
+        override fun createFromParcel(parcel: Parcel): Image {
+            return Image(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Image?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
