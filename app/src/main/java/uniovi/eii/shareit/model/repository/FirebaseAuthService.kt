a74e6a801a1e8f46aa9de40ed0d529dee5d301ce
Abel <abelmhov7l@gmail.com>
2024-10-10 03:24:50 +0200

    Añadida funcionalidad de Firestore para la creación de usuarios (A parte de Auth, se crea un archivo en Firestore para almacenar los datos del usuairo)

--- app/src/main/java/uniovi/eii/shareit/model/repository/FirebaseAuthService.kt
+++ app/src/main/java/uniovi/eii/shareit/model/repository/FirebaseAuthService.kt
@@ -3,6 +3,7 @@ package uniovi.eii.shareit.model.repository
 import android.util.Log
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
+import com.google.firebase.firestore.firestore
 import kotlinx.coroutines.tasks.await
 import uniovi.eii.shareit.viewModel.LoginViewModel.LoginResult
 import uniovi.eii.shareit.viewModel.SignUpViewModel.SignUpResult
@@ -14,7 +15,8 @@ object FirebaseAuthService {
     suspend fun createUserWithEmailAndPassword(email: String, password: String): SignUpResult {
         return try {
             with(auth.createUserWithEmailAndPassword(email, password).await()) {
-                Log.d(TAG, "createUserWithEmail:success\nCreated User ID: ${user?.uid}")
+                Log.d(TAG, "createUserWithEmail:success\nCreated User ID: ${user!!.uid}")
+                saveCreatedUserInFirestore(user!!.uid, email)
                 auth.signOut()
                 SignUpResult(true)
             }
@@ -24,10 +26,26 @@ object FirebaseAuthService {
         }
     }
 
+    private suspend fun saveCreatedUserInFirestore(userId: String, email: String) {
+        val db = Firebase.firestore
+        val user = hashMapOf(
+            "id" to userId,
+            "email" to email
+        )
+        db.collection("users")
+            .document(userId)
+            .set(user).addOnCompleteListener {
+                if (it.isSuccessful)
+                    Log.d(TAG, "saveCreatedUserInFirestore:success")
+                else
+                    Log.e(TAG, "saveCreatedUserInFirestore:failure", it.exception)
+            }.await()
+    }
+
     suspend fun signInWithEmailAndPassword(email: String, password: String): LoginResult {
         return try {
             with(auth.signInWithEmailAndPassword(email, password).await()) {
-                Log.d(TAG, "signInWithEmail:success\nLoged User ID: ${user?.uid}")
+                Log.d(TAG, "signInWithEmail:success\nLoged User ID: ${user!!.uid}")
                 LoginResult(true)
             }
         } catch (e: Exception) {
